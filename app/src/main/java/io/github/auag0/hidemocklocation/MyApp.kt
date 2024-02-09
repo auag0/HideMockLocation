06ee0c59f2dd8e5417ab5378829252bda2e02b1a
anago <outpe1119@gmail.com>
2024-02-10 02:06:40 +0900

    remove debug log line

--- app/src/main/java/io/github/auag0/hidemocklocation/MyApp.kt
+++ app/src/main/java/io/github/auag0/hidemocklocation/MyApp.kt
@@ -1,13 +1,11 @@
 package io.github.auag0.hidemocklocation
 
 import android.app.Application
-import io.github.auag0.hidemocklocation.xposed.Logger
 
 class MyApp: Application() {
     companion object {
         @JvmStatic
         fun isModuleEnabled(): Boolean {
-            Logger.logD("isModuleNotActivitaled")
             return false
         }
     }
