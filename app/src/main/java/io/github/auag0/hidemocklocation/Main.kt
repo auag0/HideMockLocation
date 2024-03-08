a89207dff43c6739f9bb7b0dd64fe79cb5f3206c
anago <outpe1119@gmail.com>
2024-03-08 18:21:22 +0900

    implement hooks for the following functions -> android.location.Location ・setIsFromMockProvider() ・setMock() ・setExtras() ・set() refactor code

--- app/src/main/java/io/github/auag0/hidemocklocation/Main.kt
+++ app/src/main/java/io/github/auag0/hidemocklocation/Main.kt
@@ -2,10 +2,18 @@ package io.github.auag0.hidemocklocation
 
 import android.os.Bundle
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XC_MethodReplacement.returnConstant
+import de.robv.android.xposed.XposedBridge.hookAllMethods
+import de.robv.android.xposed.XposedHelpers.findClass
+import de.robv.android.xposed.XposedHelpers.getIntField
+import de.robv.android.xposed.XposedHelpers.getObjectField
+import de.robv.android.xposed.XposedHelpers.getStaticIntField
+import de.robv.android.xposed.XposedHelpers.setIntField
+import de.robv.android.xposed.XposedHelpers.setObjectField
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import io.github.auag0.hidemocklocation.XposedUtils.invokeOriginalMethod
-import io.github.auag0.hidemocklocation.XposedUtils.replaceMethod
 
 class Main : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
@@ -14,22 +22,60 @@ class Main : IXposedHookLoadPackage {
     }
 
     private fun hookLocationMethods(classLoader: ClassLoader) {
-        val locationClass = XposedHelpers.findClass(
+        val locationClass = findClass(
             "android.location.Location",
             classLoader
         )
         // Hooked android.location.Location isFromMockProvider()
-        replaceMethod(locationClass, "isFromMockProvider", false)
+        hookAllMethods(locationClass, "isFromMockProvider", returnConstant(false))
         // Hooked android.location.Location isMock()
-        replaceMethod(locationClass, "isMock", false)
-        // Hooked android.location.Location getExtras()
-        replaceMethod(locationClass, "getExtras") { param ->
-            val extras: Bundle? = param.invokeOriginalMethod() as? Bundle
-            if (extras?.getBoolean("mockLocation") == true) {
-                extras.putBoolean("mockLocation", false)
+        hookAllMethods(locationClass, "isMock", returnConstant(false))
+        // Hooked android.location.Location setIsFromMockProvider()
+        hookAllMethods(locationClass, "setIsFromMockProvider", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val isFromMockProvider = param.args[0] as Boolean?
+                if (isFromMockProvider == true) {
+                    param.args[0] = false
+                }
             }
-            return@replaceMethod extras
-        }
+        })
+        // Hooked android.location.Location setMock()
+        hookAllMethods(locationClass, "setMock", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val mock = param.args[0] as Boolean?
+                if (mock == true) {
+                    param.args[0] = false
+                }
+            }
+        })
+        // Hooked android.location.Location getExtras()
+        hookAllMethods(locationClass, "getExtras", object : XC_MethodReplacement() {
+            override fun replaceHookedMethod(param: MethodHookParam): Bundle? {
+                var extras: Bundle? = param.invokeOriginalMethod() as Bundle?
+                extras = getPatchedBundle(extras)
+                return extras
+            }
+        })
+        // Hooked android.location.Location setExtras()
+        hookAllMethods(locationClass, "setExtras", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val extras = param.args[0] as Bundle?
+                param.args[0] = getPatchedBundle(extras)
+            }
+        })
+        // Hooked android.location.Location set()
+        hookAllMethods(locationClass, "set", object : XC_MethodHook() {
+            val HAS_MOCK_PROVIDER_MASK = getStaticIntField(locationClass, "HAS_MOCK_PROVIDER_MASK")
+            override fun afterHookedMethod(param: MethodHookParam) {
+                var mFieldsMask = getIntField(param.thisObject, "mFieldsMask")
+                mFieldsMask = mFieldsMask and HAS_MOCK_PROVIDER_MASK.inv()
+                setIntField(param.thisObject, "mFieldsMask", mFieldsMask)
+
+                var mExtras = getObjectField(param.thisObject, "mExtras") as Bundle?
+                mExtras = getPatchedBundle(mExtras)
+                setObjectField(param.thisObject, "mockLocation", mExtras)
+            }
+        })
     }
 
     private fun hookSettingsMethods(classLoader: ClassLoader) {
@@ -41,14 +87,29 @@ class Main : IXposedHookLoadPackage {
             "android.provider.Settings.NameValueCache"
         )
         settingsClassNames.forEach {
-            val clazz = XposedHelpers.findClass(it, classLoader)
-            replaceMethod(clazz, "getStringForUser") { param ->
-                val name: String? = param.args[1] as? String
-                if (name == "mock_location") {
-                    return@replaceMethod "0"
+            val clazz = findClass(it, classLoader)
+            hookAllMethods(clazz, "getStringForUser", object : XC_MethodReplacement() {
+                override fun replaceHookedMethod(param: MethodHookParam): Any? {
+                    val name: String? = param.args[1] as? String?
+                    return when (name) {
+                        "mock_location" -> "0"
+                        else -> param.invokeOriginalMethod()
+                    }
                 }
-                return@replaceMethod param.invokeOriginalMethod()
-            }
+            })
         }
+    }
+
+    /**
+     * if "mockLocation" containsKey in the given bundle, set it to false
+     *
+     * @param origBundle original Bundle object
+     * @return Bundle with "mockLocation" set to false
+     */
+    private fun getPatchedBundle(origBundle: Bundle?): Bundle? {
+        if (origBundle?.containsKey("mockLocation") == true) {
+            origBundle.putBoolean("mockLocation", false)
+        }
+        return origBundle
     }
 }
--- app/src/main/java/io/github/auag0/hidemocklocation/XposedUtils.kt
+++ app/src/main/java/io/github/auag0/hidemocklocation/XposedUtils.kt
@@ -1,7 +1,6 @@
 package io.github.auag0.hidemocklocation
 
 import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedBridge
 
 object XposedUtils {
@@ -11,18 +10,5 @@ object XposedUtils {
             this.thisObject,
             this.args
         )
-    }
-
-    fun replaceMethod(
-        clazz: Class<*>,
-        methodName: String,
-        returnValue: Any? = null,
-        replacement: (param: XC_MethodHook.MethodHookParam) -> Any? = { null }
-    ) {
-        XposedBridge.hookAllMethods(clazz, methodName, object : XC_MethodReplacement() {
-            override fun replaceHookedMethod(param: MethodHookParam): Any? {
-                return replacement(param) ?: returnValue
-            }
-        })
     }
 }
